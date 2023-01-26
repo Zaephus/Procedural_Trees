b489224339eb316a397ab7e97e656927b68dad5b
Zaephus14 <rozendaalmarc@gmail.com>
2023-01-26 15:33:31 +0100

    Small trunk gen update

--- Procedural Trees/Assets/01_Scripts/Tree.cs
+++ Procedural Trees/Assets/01_Scripts/Tree.cs
@@ -6,131 +6,177 @@ using UnityEngine;
 public class Tree : MonoBehaviour {
 
     [SerializeField]
-    private Material treeMaterial;
+    private bool drawGizmos;
 
-    [SerializeField]
-    private MeshFilter meshFilter;
-
+    [SerializeField, Range(3, 36)]
+    private int vertexResolution;
     [SerializeField]
     private float trunkSize;
     [SerializeField]
     private float height;
+    [SerializeField, Range(2, 36)]
+    private int heightResolution;
 
-    //private List<Vector3> points = new Vector3();
+    #region Dependencies
+    [Header("Dependencies")]
+    [SerializeField]
+    private Material treeMaterial;
+    [SerializeField]
+    private MeshFilter meshFilter;
+    #endregion
 
+    #region Main Parameters
+    [Header("Main Parameters")]
+    [SerializeField]
+    private Shape shape;
+    [SerializeField]
+    private float baseSize;
+    [SerializeField]
+    private float scale;
+    [SerializeField]
+    private float scaleVariance;
+    [SerializeField]
+    private float zScale;
+    [SerializeField]
+    private float zScaleVariance;
+    [SerializeField]
+    private float ratio;
+    [SerializeField]
+    private float ratioPower;
+    [SerializeField]
+    private int lobes;
+    [SerializeField]
+    private float lobeDepth;
+    [SerializeField]
+    private float flare;
+    #endregion
+
+    #region Base Parameters
+    [Header("Base Parameters")]
+    [SerializeField]
+    private float baseScale;
+    [SerializeField]
+    private float baseScaleVariance;
+    [SerializeField]
+    private float baseLength;
+    [SerializeField]
+    private float baseLengthVariance;
+    [SerializeField]
+    private float baseTaper;
+    [SerializeField]
+    private int baseSplits;
+    [SerializeField]
+    private int basesegmentSplits;
+    [SerializeField]
+    private float basesplitAngle;
+    [SerializeField]
+    private float baseSplitAngleVariance;
+    [SerializeField]
+    private float baseCurveResolution;
+    [SerializeField]
+    private float baseCurve;
+    [SerializeField]
+    private float baseCurveBack;
+    [SerializeField]
+    private float baseCurveVariance;
+    #endregion
+
+    #region Branch Parameters
+    [Header("Branch Parameters")]
+    [SerializeField]
+    private BranchData[] branchDatas;
+    #endregion
+    private List<Vector3> points = new List<Vector3>();
     private List<Vector3> vertices = new List<Vector3>();
 
-    [Range(0,1)]
-    public int k = 0;
-    [Range(0,7)]
-    public int l = 3;
+    private enum Shape {
+        Conical = 0,
+        Spherical = 1,
+        HemiSpherical = 2,
+        Cylindrical = 3,
+        TaperedCylindrical = 4,
+        Flame = 5,
+        InverseConical = 6,
+        TendFlame = 7
+    }
 
-    public int[] triangles;
+    [System.Serializable]
+    private class BranchData {
+
+        public float downAngle;
+        public float downAngleVariance;
+        public float rotate;
+        public float rotateVariance;
+        public int branches;
+        public float length;
+        public float lengthVariance;
+        public float taper;
+        public int segmentSplits;
+        public float splitAngle;
+        public float splitAngleVariance;
+        public float curveResolution;
+        public float curve;
+        public float curveBack;
+        public float curveVariation;
+
+    }
     
     public void Generate() {
 
+        points.Clear();
         vertices.Clear();
         
         meshFilter.sharedMesh = new Mesh();
         meshFilter.GetComponent<MeshRenderer>().sharedMaterial = treeMaterial;
 
-        Vector3[] points = {
-            transform.position,
-            transform.position + new Vector3(Random.Range(-trunkSize, trunkSize), height/2, Random.Range(-trunkSize, trunkSize)),
-            transform.position + new Vector3(Random.Range(-trunkSize, trunkSize), height, Random.Range(-trunkSize, trunkSize))
-        };
+        // points =  new List<Vector3> {
+        //     transform.position,
+        //     transform.position + new Vector3(Random.Range(-trunkSize, trunkSize), height/2, Random.Range(-trunkSize, trunkSize)),
+        //     transform.position + new Vector3(Random.Range(-trunkSize, trunkSize), height, Random.Range(-trunkSize, trunkSize))
+        // };
 
-        //Vector3[] vertices = new Vector3[points.Length * 4];
-        triangles = new int[points.Length * 16];
+        points.Add(transform.position);
+        for(int i = 1; i < heightResolution; i++) {
+            int inverseIndex = heightResolution-i;
+            points.Add(transform.position + new Vector3(Random.Range(-trunkSize, trunkSize) / inverseIndex, 
+                                                        i * (height/heightResolution),
+                                                        Random.Range(-trunkSize, trunkSize) / inverseIndex));
+        }
 
-        for(int i = 0; i < points.Length; i++) {
+        //Vector3[] vertices = new Vector3[points.Length * vertexResolution];
 
-            int inverseIndex = points.Length - i;
+        for(int i = 0; i < points.Count; i++) {
 
-            // vertices[i*4] = points[i] + new Vector3(inverseIndex * trunkSize, 0, 0);
-            // vertices[i*4 + 1] = points[i] + new Vector3(0, 0, inverseIndex * trunkSize);
-            // vertices[i*4 + 2] = points[i] + new Vector3(-inverseIndex * trunkSize, 0, 0);
-            // vertices[i*4 + 3] = points[i] + new Vector3(0, 0, -inverseIndex * trunkSize);
+            int inverseIndex = points.Count - i;
 
-            vertices.Add(points[i] + new Vector3(inverseIndex * trunkSize, 0, 0));
-            vertices.Add(points[i] + new Vector3(0, 0, inverseIndex * trunkSize));
-            vertices.Add(points[i] + new Vector3(-inverseIndex * trunkSize, 0, 0));
-            vertices.Add(points[i] + new Vector3(0, 0, -inverseIndex * trunkSize));
+            for(int j = 0; j < vertexResolution; j++) {
+                float currentAngle = ((2*Mathf.PI)/vertexResolution)*j;
+                vertices.Add(points[i] + (trunkSize / (i+2)) * new Vector3(Mathf.Cos(currentAngle), 0, Mathf.Sin(currentAngle)));
+            }
 
         }
 
-        // for(int i = 1; i < points.Length; i++) {
-        //     for(int j = 0; j < 4; j++) {
-
-        //         triangles[((i-1) * 6) + 0 + j] = i * 4 + j;
-        //         triangles[((i-1) * 6) + 1 + j] = i * 4 + (1 + j)%4;
-        //         triangles[((i-1) * 6) + 2 + j] = (i-1) * 4 + j;
-
-        //         // triangles[((i-1) * 6) + 3 + j] = i * 4 + (1 + j)%4;
-        //         // triangles[((i-1) * 6) + 4 + j] = (i-1) * 4 + j;
-        //         // triangles[((i-1) * 6) + 5 + j] = (i-1) * 4 + j;
-
-        //     }
-        // }
-        // triangles[k * 6 + 0] = l + (k + 1) * 4;
-        // triangles[k * 6 + 1] = l + (k + 1) * 4 - 3;
-        // triangles[k * 6 + 2] = l + (k + 1) * 4 - 4;
-
-        // triangles[k * 6 + 3] = l + (k + 1) * 4;
-        // triangles[k * 6 + 4] = l + (k + 1) * 4 + 1;
-        // triangles[k * 6 + 5] = l + (k + 1) * 4 - 3;
-
-
-        // triangles[k * 6 + 0] = l + ((k + 1) * 4) - 1;
-        // triangles[k * 6 + 1] = l + ((k + 1) * 4);
-        // triangles[k * 6 + 2] = l + ((k + 1) * 4) - 4;
-
-        // triangles[k * 6 + 3] = l + ((k + 1) * 4);
-        // triangles[k * 6 + 4] = l + ((k + 1) * 4) - 3;
-        // triangles[k * 6 + 5] = l + ((k + 1) * 4) - 4;
-
-        // triangles[l * 6 + 0] = l + 4 - 1;
-        // triangles[l * 6 + 1] = l + 4;
-        // triangles[l * 6 + 2] = l + 4 - 4;
-
-        // triangles[l * 6 + 3] = l + 4;
-        // triangles[l * 6 + 4] = l + 4 - 3;
-        // triangles[l * 6 + 5] = l + 4 - 4;
-
-        // Debug.Log(l + (k + 1) * 4);
-        // Debug.Log(l + (k + 1) * 4 + 1);
-        // Debug.Log(l + (k + 1) * 4 - 3);
-
-        // for(int i = 0; i < points.Length-1; i++) {
-        //     for(int j = 0; j < 4; j++) {
-        //         triangles[i * j * 6 + 0] = j + (i + 1) * 4 - 1;
-        //         triangles[i * j * 6 + 1] = j + (i + 1) * 4;
-        //         triangles[i * j * 6 + 2] = j + (i + 1) * 4 - 4;
-    
-        //         triangles[i * j * 6 + 3] = j + (i + 1) * 4;
-        //         triangles[i * j * 6 + 4] = j + (i + 1) * 4 - 3;
-        //         triangles[i * j * 6 + 5] = j + (i + 1) * 4 - 4;
-        //     }
-        // // }
+        
+        int[] triangles = new int[(vertices.Count - vertexResolution) * 6];
 
         for(int i = 0; i < triangles.Length/6; i++) {
-            triangles[i * 6 + 0] = i + 4 - 1;
-            triangles[i * 6 + 1] = i + 4;
-            triangles[i * 6 + 2] = i + 4 - 4;
+            triangles[i * 6 + 0] = i + vertexResolution - 1;
+            triangles[i * 6 + 1] = i + vertexResolution;
+            triangles[i * 6 + 2] = i + vertexResolution - vertexResolution;
 
-            triangles[i * 6 + 3] = i + 4;
-            triangles[i * 6 + 4] = i + 4 - 3;
-            triangles[i * 6 + 5] = i + 4 - 4;
+            triangles[i * 6 + 3] = i + vertexResolution;
+            triangles[i * 6 + 4] = i + vertexResolution - vertexResolution + 1;
+            triangles[i * 6 + 5] = i + vertexResolution - vertexResolution;
         }
 
-        // Vector3[] finalVertices = new Vector3[triangles.Length];
-        // for (int i = 0; i < triangles.Length; i++) {
-        //     finalVertices[i] = vertices[triangles[i]];
-        //     triangles[i] = i;
-        // }
+        Vector3[] finalVertices = new Vector3[triangles.Length];
+        for (int i = 0; i < triangles.Length; i++) {
+            finalVertices[i] = vertices[triangles[i]];
+            triangles[i] = i;
+        }
 
         meshFilter.sharedMesh.Clear();
-        meshFilter.sharedMesh.vertices = vertices.ToArray();
+        meshFilter.sharedMesh.vertices = finalVertices;
         meshFilter.sharedMesh.triangles = triangles;
         meshFilter.sharedMesh.RecalculateNormals();
         meshFilter.sharedMesh.RecalculateBounds();
@@ -139,11 +185,19 @@ public class Tree : MonoBehaviour {
 
     private void OnDrawGizmos() {
         
-        Gizmos.color = Color.blue;
+        if(drawGizmos) {
+            Gizmos.color = Color.blue;
 
-        foreach(Vector3 vec in vertices) {
-            Gizmos.DrawSphere(vec, 0.2f);
+            foreach(Vector3 vert in vertices) {
+                Gizmos.DrawSphere(vert, 0.2f);
+            }
+
+            Gizmos.color = Color.green;
+            for(int i = 1; i < points.Count; i++) {
+                Gizmos.DrawLine(points[i-1], points[i]);
+            }
         }
+
     }
 
 }
